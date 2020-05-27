20e2cb4db56e0edad5d6e497b26f62ec6a9ede8d
Ptr <petarman96@abv.bg>
2020-05-27 13:32:50 +0300

    test

--- src/Display.java
+++ src/Display.java
@@ -1,38 +1,31 @@
 import javax.swing.*;
 
 public class Display {
-    private JFrame frame;
-    private JTextArea txtArea;
+    private SideComponents sideComponents = new SideComponents();
     private JMenuBar menuBar = new JMenuBar();
     private FileMenu fileMenu = new FileMenu();
     private EditMenu editMenu = new EditMenu();
 
-    public void setTxtArea(JTextArea txtArea) {
-        this.txtArea = txtArea;
-    }
-
     public void createWindow() {
         setsDefaultSettings();
-        txtArea = new JTextArea(10, 10);
-        frame.add(txtArea);
-        txtArea.setText("sdasdasdasfsdgdfhgdfh");
+        sideComponents.setTxtArea(new JTextArea(10, 10));
+        sideComponents.getFrame().add(sideComponents.getTxtArea());
+        //sideComponents.getTxtArea().setText("sdasdasdasfsdgdfhgdfh");
         createsMenuBar();
     }
 
     public void setsDefaultSettings() {
-        frame = new JFrame("Text Editor");
-        frame.pack();
-        frame.setSize(100, 100);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
+        sideComponents.setFrame(new JFrame("Text Editor"));
+        sideComponents.getFrame().pack();
+        sideComponents.getFrame().setSize(100, 100);
+        sideComponents.getFrame().setVisible(true);
+        sideComponents.getFrame().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public void createsMenuBar() {
         menuBar.add(fileMenu.getsMenu());
         menuBar.add(editMenu.getsMenu());
-        frame.setJMenuBar(menuBar);
+        sideComponents.getFrame().setJMenuBar(menuBar);
     }
 
 
--- src/FileMenu.java
+++ src/FileMenu.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class FileMenu implements Menu {
-
     private JMenu fileMenu = new JMenu("File");
     private JMenuItem openOption = new JMenuItem("Open");
-
-
+    private SideComponents sideComponents = new SideComponents();
 
     @Override
     public void createsMenu() {
@@ -33,7 +34,44 @@ public class FileMenu implements Menu {
     public void listenActions(){
         openOption.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
-                
+                JFileChooser j = new JFileChooser("f:");
+
+                // Invoke the showsOpenDialog function to show the save dialog
+                int r = j.showOpenDialog(null);
+
+                // If the user selects a file
+                if (r == JFileChooser.APPROVE_OPTION) {
+                    // Set the label to the path of the selected directory
+                    File fi = new File(j.getSelectedFile().getAbsolutePath());
+
+                    try {
+                        // String
+                        String s1 = "", sl = "";
+
+                        // File reader
+                        FileReader fr = new FileReader(fi);
+
+                        // Buffered reader
+                        BufferedReader br = new BufferedReader(fr);
+
+                        // Initilize sl
+                        sl = br.readLine();
+
+                        // Take the input from the file
+                        while ((s1 = br.readLine()) != null) {
+                            sl = sl + "\n" + s1;
+                        }
+
+                        // Set the text
+                        txtArea.setText(sl);
+                    }
+                    catch (Exception evt) {
+                        JOptionPane.showMessageDialog(sideComponents.getFrame(), evt.getMessage());
+                    }
+                }
+                // If the user cancelled the operation
+                else
+                    JOptionPane.showMessageDialog(sideComponents.getFrame(), "the user cancelled the operation");
             }
         });
     }
